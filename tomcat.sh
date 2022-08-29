15b75af2e381f93cce0cbd2799cf2faae9ff1f3a
NAZIYASHAIK1695 <101784632+NAZIYASHAIK1695@users.noreply.github.com>
2022-08-29 10:32:26 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,5 +2,5 @@
 sudo yum update
 sudo yum -y install java-1.8* java-11* git 
 sudo git clone https://github.com/NAZIYASHAIK1695/apache-tomcat-7.0.94-.git
-sudo cd apache-tomcat-7.0.94/bin
+sudo cd apache-tomcat-7.0.94-/bin
 sudo ./startup.sh 
