fb7cd82be0d669384da409ab24993ae3b6524812
NAZIYASHAIK1695 <101784632+NAZIYASHAIK1695@users.noreply.github.com>
2022-10-24 21:38:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIAVXXCM2QLMVNW7IU6"
-  secret_key = "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"
+  access_key = "AKIA"
+  secret_key = "IGBjdSgEcyL1+SJyTWjcWF"
 }
